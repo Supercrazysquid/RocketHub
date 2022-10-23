37750441eb36a8660a4133a137252949b754f9be
Supercrazysquid <supercrazysquid@gmail.com>
2022-10-23 12:58:12 -0400

    Created listplayers & clearplayer

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ client = commands.Bot(command_prefix=commandPrefix, intents=givenIntents)
 # Events
 @client.command(name="add")
 @commands.has_permissions(administrator=True)
-async def on_message(ctx, rlUser, discord):
+async def add_command(ctx, rlUser, discord):
 	jsonData = {}
 
 	with open(playersFile, "r") as players:
@@ -33,6 +33,35 @@ async def on_message(ctx, rlUser, discord):
 		json.dump(jsonData, players)
 
 	await ctx.message.reply(f"Added {rlUser}")
+ 
+@client.command(name="listplayers")
+@commands.has_permissions(administrator=True)
+async def listplayers_command(ctx):
+	jsonData = {}
+
+	with open(playersFile, "r") as players:
+		jsonData = json.loads(players.read())
+
+	playerList = ""
+
+	for player in jsonData:
+		playerList += f"RL Username: {player},\n"
+	
+	sender = ctx.author
+
+	await sender.send("Player list\n" + playerList)
+
+@client.command(name="clearplayers")
+@commands.has_permissions(administrator=True)
+async def clearplayers_command(ctx):
+    replace = "{}"
+    
+    with open(playersFile, "w") as players:
+        players.write(replace)
+    
+    sender = ctx.author
+    
+    await ctx.reply("Cleared the player list")
 
 # Main
 def main():
